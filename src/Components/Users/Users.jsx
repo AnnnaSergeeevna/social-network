e842b142a01c7a03d35de57e807120f09514f6cb
Aa9214057525 <anna.s.lipatova@gmail.com>
2023-02-15 12:33:07 +0100

    with pagination

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -5,39 +5,62 @@ import userPhoto from '../Users/userPhoto.png'
 
 class Users extends React.Component {
     componentDidMount() {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-        this.props.setUsers(response.data.items)
-    })
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+            this.props.setTotalUsersCount(response.data.totalCount)
+        })
     }
-render () {
-    return <div>
-        {
-        this.props.users.map(u => <div key={u.id}>
-                <div className={styles.userinf}>
-                    <span>
-                        <div>
-                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.usrPhoto} />
-                        </div>
-                        <div>
-                            {u.followed
-                                ? <button className={styles.button} onClick={() => { this.props.unfollow(u.id) }} >Unfollow</button>
-                                : <button className={styles.button} onClick={() => { this.props.follow(u.id) }}>Follow</button>}
-                        </div>
-                    </span>
-                    <span>
-                        <div className={styles.usrdet} >
-                            <div>{u.name}</div>
-                            <div>{u.status}</div>
-                            {/* <h3>{u.location.city}</h3>
-                            <div>{u.location.country}</div> */}
-                        </div>
-                    </span>
-                </div>
-            </div>
-            )
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+        })
+    }
+
+    render() {
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
+        let pages = []
+        let firstPage = this.props.currentPage - 10
+        let lastPage = this.props.currentPage + 10
+        if (firstPage < 1) {firstPage = 1}
+        if (lastPage > pagesCount) {lastPage = pagesCount}
+        for (let i = firstPage; i <= lastPage; i++) {
+            pages.push(i)
         }
-    </div >
-}
+        return <div>
+            <div>
+                {pages.map(p => {
+                    return <span className={this.props.currentPage === p ? styles.selectedPage : styles.pagesNmbrs}
+                        onClick={() => { this.onPageChanged(p) }}>{p+' '}</span>
+                })}
+            </div>
+            {
+                this.props.users.map(u => <div key={u.id}>
+                    <div className={styles.userinf}>
+                        <span>
+                            <div>
+                                <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.usrPhoto} />
+                            </div>
+                            <div>
+                                {u.followed
+                                    ? <button className={styles.button} onClick={() => { this.props.unfollow(u.id) }} >Unfollow</button>
+                                    : <button className={styles.button} onClick={() => { this.props.follow(u.id) }}>Follow</button>}
+                            </div>
+                        </span>
+                        <span>
+                            <div className={styles.usrdet} >
+                                <div>{u.name}</div>
+                                <div>{u.status}</div>
+                                {/* <h3>{u.location.city}</h3>
+                            <div>{u.location.country}</div> */}
+                            </div>
+                        </span>
+                    </div>
+                </div>
+                )
+            }
+        </div >
+    }
 }
 export default Users
 
--- src/Redux/usersReduser.js
+++ src/Redux/usersReduser.js
@@ -1,11 +1,14 @@
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET_USERS'
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
 
 let initialState = {
-    users: [
-        {id: 1, photos: { "small": './1.png', "large": null }, followed: true, name: 'Maroon', status: 'Null', location: {city: 'Florence', country: 'Italy'}},
-       ]
+    users: [ ],
+    pageSize: 10,
+    totalUsersCount: 50,
+    currentPage: 2
     }
 const usersReduser = (state = initialState, action) => {
     switch (action.type) {
@@ -29,9 +32,16 @@ const usersReduser = (state = initialState, action) => {
                     return u
                 })
             }
-        case SET_USERS: {
-            return { ...state, users: [...state.users, ...action.users] }
-        }
+            case SET_USERS: {
+                return { ...state, users: action.users }
+            }
+            case SET_CURRENT_PAGE: {
+                return { ...state, currentPage: action.currentPage }
+            }
+            case SET_TOTAL_USERS_COUNT: {
+                return { ...state, totalUsersCount: action.count }
+  
+            }
         default:
             return state
     }
@@ -39,5 +49,7 @@ const usersReduser = (state = initialState, action) => {
 export const followAC = (userId) => ({ type: FOLLOW, userId })
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId })
 export const setUsersAC = (users) => ({type: SET_USERS, users})
+export const setcurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage})
+export const setTotalUsersCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount})
 
 export default usersReduser
