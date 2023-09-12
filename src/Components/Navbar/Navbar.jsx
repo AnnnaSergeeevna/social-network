0959012b71eb92e355b268c956fcbba60ac74723
Aa9214057525 <anna.s.lipatova@gmail.com>
2023-09-12 19:26:15 +0200

    add build

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = (props) => {
         <div className={classes.item}><NavLink to="/dialogs" className={navData => navData.isActive ? classes.active : classes.item}>Messages</NavLink></div>
         <div className={classes.item}><NavLink to="/users" className={navData => navData.isActive ? classes.active : classes.item}>Users</NavLink></div>
         <div className={classes.item}><NavLink to="/payment" className={navData => navData.isActive ? classes.active : classes.item}>Payment</NavLink></div>
-        <div className={classes.item}><NavLink to="/canvas" className={navData => navData.isActive ? classes.active : classes.item}>Canvas</NavLink></div>
+        {/* <div className={classes.item}><NavLink to="/canvas" className={navData => navData.isActive ? classes.active : classes.item}>Canvas</NavLink></div> */}
         <div className={classes.item}><NavLink to="/news" className={navData => navData.isActive ? classes.active : classes.item}>News</NavLink></div>
         <div className={classes.item}><NavLink to="/music" className={navData => navData.isActive ? classes.active : classes.item}>Music</NavLink></div>
         <div className={classes.item}><NavLink to="/settings" className={navData => navData.isActive ? classes.active : classes.item}>Settings</NavLink></div>
